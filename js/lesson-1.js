66d168f6b7cd7fecb87f6376bea01b1c24abefa5
vladickjerseyclub <pakizhj@gmail.com>
2024-03-24 10:25:51 +0100

    lesson-3

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -19,11 +19,42 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
 // 70 === 01:10
 
-const answer = prompt("Введіть кількість хвилин");
-let hours = Math.floor(answer / 60);
-let minutes = answer % 60;
+// const answer = prompt("Введіть кількість хвилин");
+// let hours = Math.floor(answer / 60);
+// let minutes = answer % 60;
 
-hours = String(hours).padStart(2, "0");  
-minutes = String(minutes).padStart(2, "0"); 
+// hours = String(hours).padStart(2, "0");  
+// minutes = String(minutes).padStart(2, "0"); 
 
-console.log(`${hours}:${minutes}`);
+// console.log(`${hours}:${minutes}`);
+
+//Напишіть цикл, який виводить в консоль
+//числа от max до min по зменьшенню
+//Виведіть в консоль суму усіх парних чисел
+
+// function counter (max, min) {
+//     let sum = 0;
+//     for (let i = max; i >= min; i--) {
+//         console.log(i);
+//         if (i % 2 === 0) {
+//             sum += i;
+//         }
+//     }
+//     return sum;
+// }
+
+// console.log(counter (10, 1));
+
+const max = 10;
+const min = 1;
+let counter = max;
+let sum = 0;
+while (counter >= min) {
+    console.log(counter);
+    if (counter % 2 === 0) {
+        sum += counter;
+    }
+    counter -= 1;
+}
+
+console.log(sum);
