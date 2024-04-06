95b8da5e87ff76c38f7d18d4a6b0b73b327816ae
Anastasiia Tarasiuk <111896564+anastasiiatarasiuk@users.noreply.github.com>
2024-04-06 12:04:33 +0300

    task2.3

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -17,7 +17,7 @@
 //     for (let i = 0; i < array.length; i++){
 //         console.log(`${i+1} - ${array[i]}`);
 //     }
-        
+
 // };
 
 // logItems(styles);
@@ -44,10 +44,7 @@
 // return "Користувач не знайден";
 // }
 
-
 // console.log(checkLogins(logins));
-
-
 
 //Напиши скрипт, який для об'єкту user,
 //послідовно:
@@ -56,16 +53,44 @@
 //3 замінить значення premium на false
 //4 виводить зміст об'єкта users у форматі
 //'<ключ>:<значення>' використовуя Object.keys() та for...of
-const user = {
-  name: "John",
-  age: 20,
-  hobby: "tenis",
-  premium: true,
-};
-user.mood = 'happy';
-user.hobby = 'skydiving';
-user.premium = false;
-const userKeys = Object.keys(user);
-for (const key of userKeys) {
-    console.log(`${key}:${user[key]}`);
+// const user = {
+//   name: "John",
+//   age: 20,
+//   hobby: "tenis",
+//   premium: true,
+// };
+// user.mood = 'happy';
+// user.hobby = 'skydiving';
+// user.premium = false;
+// const userKeys = Object.keys(user);
+// for (const key of userKeys) {
+//     console.log(`${key}:${user[key]}`);
+// }
+
+//3. Напишіть ф-цію calcTotalPrice(fruits, fruitName),
+//яка приймає массив об'єктів і
+//рядок з назвою фрукта.
+//Функція рахує і повертає загальну вартість фрукта
+//з таким ім'ям, ціною та кількістю з об'єкта
+const fruits = [
+  { name: "Яблуко", price: 45, quantity: 7 },
+  { name: "Апельсин", price: 60, quantity: 4 },
+  { name: "Банан", price: 125, quantity: 8 },
+  { name: "Груша", price: 350, quantity: 2 },
+  { name: "Виноград", price: 440, quantity: 3 },
+  { name: "Яблуко", price: 45, quantity: 7 },
+];
+function calcTotalPrice(fruits, fruitName) {
+  let totalPrice = 0;
+  for (const fruit of fruits) {
+    if (fruit.name === fruitName) {
+      totalPrice += fruit.price * fruit.quantity;
+    }
+  }
+  if (totalPrice === 0) {
+    return `${fruitName} not found`;
+  }
+  return totalPrice;
 }
+console.log(calcTotalPrice(fruits, "Яблуко"));
+console.log(calcTotalPrice(fruits, "Яблук"));
