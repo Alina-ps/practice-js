5433823a86df701b7cf5c9a293daa54d7788557d
oleksandr-sulima <sulima.sasha@gmail.com>
2024-04-20 10:08:40 +0200

    task-3.2

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -18,23 +18,123 @@
 // callAction(() => console.log(this.price));
 
 // 4. Функція askPassword приймає 2 колбека і викликає 1 із них в залежності від password
-function askPassword(ok, fail) {
-  let password = prompt("Password?");
-  if (password === "admin") ok();
-  else fail();
-}
-// Створи об 'єкт user з властивостю name і двома методами
-// loginOk() і loginFail()
-// ці методи виводять в консоль повідомлення у форматі
-// "<name> logged in" та "<name> failed to log in" відповідно
-// зроби виклик функції askPassword, прив'язавши в якості аргументів методи об'єкта
-const user = {
-  name: "Alice",
-  loginOk() {
-    console.log(`${this.name} logged in`);
+// function askPassword(ok, fail) {
+//   let password = prompt("Password?");
+//   if (password === "admin") ok();
+//   else fail();
+// }
+// // Створи об 'єкт user з властивостю name і двома методами
+// // loginOk() і loginFail()
+// // ці методи виводять в консоль повідомлення у форматі
+// // "<name> logged in" та "<name> failed to log in" відповідно
+// // зроби виклик функції askPassword, прив'язавши в якості аргументів методи об'єкта
+// const user = {
+//   name: "Alice",
+//   loginOk() {
+//     console.log(`${this.name} logged in`);
+//   },
+//   loginFail() {
+//     console.log(`${this.name} failed to log in`);
+//   },
+// };
+// askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+
+const users = [
+  {
+    id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+    name: "Moore Hensley",
+    email: "moorehensley@indexia.com",
+    eyeColor: "blue",
+    friends: ["Sharron Pace"],
+    isActive: false,
+    balance: 2811,
+    skills: ["ipsum", "lorem"],
+    gender: "male",
+    age: 37,
   },
-  loginFail() {
-    console.log(`${this.name} failed to log in`);
+  {
+    id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+    name: "Sharlene Bush",
+    email: "sharlenebush@tubesys.com",
+    eyeColor: "blue",
+    friends: ["Briana Decker", "Sharron Pace"],
+    isActive: true,
+    balance: 3821,
+    skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+    gender: "female",
+    age: 34,
   },
-};
-askPassword(user.loginOk.bind(user), user.loginFail.bind(user));
+  {
+    id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
+    name: "Ross Vazquez",
+    email: "rossvazquez@xinware.com",
+    eyeColor: "green",
+    friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+    isActive: false,
+    balance: 3793,
+    skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+    gender: "male",
+    age: 24,
+  },
+  {
+    id: "249b6175-5c30-44c6-b154-f120923736f5",
+    name: "Elma Head",
+    email: "elmahead@omatom.com",
+    eyeColor: "green",
+    friends: ["Goldie Gentry", "Aisha Tran"],
+    isActive: true,
+    balance: 2278,
+    skills: ["adipisicing", "irure", "velit"],
+    gender: "female",
+    age: 21,
+  },
+  {
+    id: "334f8cb3-eb04-45e6-abf4-4935dd439b70",
+    name: "Carey Barr",
+    email: "careybarr@nurali.com",
+    eyeColor: "blue",
+    friends: ["Jordan Sampson", "Eddie Strong"],
+    isActive: true,
+    balance: 3951,
+    skills: ["ex", "culpa", "nostrud"],
+    gender: "male",
+    age: 27,
+  },
+  {
+    id: "150b00fb-dd82-427d-9faf-2879ea87c695",
+    name: "Blackburn Dotson",
+    email: "blackburndotson@furnigeer.com",
+    eyeColor: "brown",
+    friends: ["Jacklyn Lucas", "Linda Chapman"],
+    isActive: false,
+    balance: 1498,
+    skills: ["non", "amet", "ipsum"],
+    gender: "male",
+    age: 38,
+  },
+  {
+    id: "e1bf46ab-7168-491e-925e-f01e21394812",
+    name: "Sheree Anthony",
+    email: "shereeanthony@kog.com",
+    eyeColor: "brown",
+    friends: ["Goldie Gentry", "Briana Decker"],
+    isActive: true,
+    balance: 2764,
+    skills: ["lorem", "veniam", "culpa"],
+    gender: "female",
+    age: 39,
+  },
+];
+// Task 1
+// Отримати масив імен усіх користувачів (поле name).
+// console.log(getUserNames(users))
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+const getUserNames = (users) => users.map((user) => user.name);
+console.log(getUserNames(users));
+
+// зробити функцію універсальною, щоб вона повертала
+// масив будь-якої заданої властивості
+
+const getUserValues = (users, prop) => users.map((user) => user[prop]);
+console.log(getUserValues(users, "email"));
