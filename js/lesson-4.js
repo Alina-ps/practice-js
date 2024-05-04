703160eaefb194d3a933b4076f086012af873fed
Eva-Mykhno <154619097+Eva-Mykhno@users.noreply.github.com>
2024-05-04 11:22:47 +0300

    task-4-2

--- js/lesson-4.js
+++ js/lesson-4.js
@@ -4,22 +4,39 @@
 // Парним лі вказати жовтий фон, непарним синій
 // Використовуй createElement
 
-
 const list = document.createElement("ol");
 const btnAdd = document.createElement("button");
 const btnRemove = document.createElement("button");
 const input = document.createElement("input");
-btnAdd.textContent = "Add"
-btnRemove.textContent = "Remove"
+btnAdd.textContent = "Add";
+btnRemove.textContent = "Remove";
 document.body.append(input, btnAdd, btnRemove, list);
 
 btnAdd.addEventListener("click", createItem);
+btnRemove.addEventListener("click", removeItem);
+
 function createItem() {
-    const inputValue = input.value.trim();
-    if (!inputValue)
-        return;
-    const li = document.createElement("li");
-    li.textContent = inputValue;
-    list.append(li);
-    input.value = "";
-};
+  const inputValue = input.value.trim();
+  if (!inputValue) return;
+  const li = document.createElement("li");
+  li.textContent = inputValue;
+  list.append(li);
+  input.value = "";
+  btnRemove.disabled = false;
+
+  //   if (list.children.length % 2 === 0) {
+  //     li.style.backgroundColor = "yellow";
+  //   } else {
+  //     li.style.backgroundColor = "blue";
+  //   }
+
+  li.style.backgroundColor = list.children.length % 2 === 0 ? "yellow" : "blue";
+}
+
+function removeItem() {
+  if (!list.lastChild) {
+    btnRemove.disabled = true;
+    return;
+  }
+  list.lastChild.remove();
+}
